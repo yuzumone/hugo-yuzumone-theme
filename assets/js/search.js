f2a58a0c92aac059e70c724c5ca6d7d8fe0db081
yuzumone <yuzumone@outlook.com>
2025-04-25 19:21:37 +0900

    tweak

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,14 +2,17 @@ $(document).ready(function() {
     $.getJSON("/index.json", function(data) {
         let resultsContainer = $("#search-results");
         data.forEach(function(item) {
-            resultsContainer.append('<li><a href="' + item.url + '">' + item.title + '</a></li>');
+            let div = '<div><a class="font-large" href="' + item.url + '">' + item.title + '</a><p class="article-description">' + item.body +'</p></div>';
+            resultsContainer.append(div);
         })
 
         function search(query) {
             let result = [];
+            const regex = new RegExp(query.replace(/^([▽▼])/, ''), 'i');
             for (i = 0; i < data.length; i++) {
-                if (data[i].body.match(new RegExp(query, 'i'))) {
-                    result.push(data[i]);
+                let pos = data[i].body.search(regex);
+                if (pos != -1) {
+                    result.push({data: data[i], pos: pos});
                 }
             }
             return result;
@@ -21,7 +24,10 @@ $(document).ready(function() {
             let results = search(query);
             if (results.length > 0) {
                 results.forEach(function(item) {
-                    resultsContainer.append('<li><a href="' + item.url + '">' + item.title + '</a></li>');
+                    let desc = '<p class="article-description">' + item['data'].body.slice(item['pos']) +'</p>';
+                    let a = '<a class="font-large" href="' + item['data'].url + '">' + item['data'].title + '</a>';
+                    let div = '<div>' + a + desc + '</div>';
+                    resultsContainer.append(div);
                 })
             } else if (query.length > 0) {
                 resultsContainer.append('<li>検索結果はありません</li>');
